68f87af9be71a4da29610df58bf837ae658a89f2
Volodymyr Butko <turb0bur@ukr.net>
2019-11-23 00:48:11 +0200

    Created database structure by adding needed migrations; added IDE folder to gitignore

---
+++ database/migrations/2019_11_22_220307_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->unsignedInteger('id');
+            $table->unsignedInteger('user_id');
+            $table->float('total');
+            $table->string('credit_card')->nullable();
+            $table->unsignedTinyInteger('payment');
+            $table->unsignedTinyInteger('delivery');
+            $table->string('order');
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
---
+++ database/migrations/2019_11_22_220317_create_discounts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDiscountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('discounts', function (Blueprint $table) {
+            $table->unsignedInteger('id');
+            $table->unsignedInteger('product_id');
+            $table->unsignedTinyInteger('type');
+            $table->unsignedTinyInteger('value');
+            $table->timestamp('start_at');
+            $table->timestamp('end_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('discounts');
+    }
+}
